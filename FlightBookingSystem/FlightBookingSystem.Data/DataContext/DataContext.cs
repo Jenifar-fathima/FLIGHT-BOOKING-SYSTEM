396dfa04db767f8dc21ba074d430d94dbede9d1a
Jeni <jenifarfathima2103@gmail.com>
2024-10-04 12:26:11 +0530

    CHANGED: Project files

--- FlightBookingSystem/FlightBookingSystem.Data/DataContext/DataContext.cs
+++ FlightBookingSystem/FlightBookingSystem.Data/DataContext/DataContext.cs
@@ -2,7 +2,6 @@
 {
     public static class DataContext
     {
-        public static  List<Passenger> Passengers { get; set; } = new List<Passenger>();
         public static List<Seat> Seats { get; set; } = new List<Seat>();
 
         public static void InitializeSeats()
--- FlightBookingSystem/FlightBookingSystem.Logic/SeatBookingBL/SeatBooking.cs
+++ FlightBookingSystem/FlightBookingSystem.Logic/SeatBookingBL/SeatBooking.cs
@@ -4,24 +4,23 @@
     {
         public void BookSeat()
         {
+            Console.WriteLine(Messages.SeatBookingTitle);
+            SeatList();
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
             Console.Write("Enter your gender: ");
             string gender = Console.ReadLine();
             Console.Write("Enter your age: ");
             int age = int.Parse(Console.ReadLine());
-            Console.WriteLine("Choose seat type (Window, Middle, Aisle): ");
-            SeatType seatType = (SeatType)Enum.Parse(typeof(SeatType), Console.ReadLine(), true);
             Console.Write("Enter seat number (e.g., 1A): ");
             string inputSeatNumber = Console.ReadLine();
             string seatNumber = inputSeatNumber + "-A";
             var passengerDTO = new PassengerDTO { PassengerName = name, Gender = gender, Age = age };
-            var seatDTO = new SeatDTO { SeatType = seatType, SeatNumber = seatNumber };
+            var seatDTO = new SeatDTO { SeatNumber = seatNumber };
 
-            if (DataContext.IsSeatAvailable(seatNumber))
+            if (DataContext.IsSeatAvailable(seatNumber) && age > 2)
             {
                 int fare = CalculateFare(age);
-                var passenger = new Passenger(passengerDTO);
                 var seat = new Seat(seatDTO)
                 {
                     Fare = fare,
@@ -29,29 +28,29 @@
                 };
                 var updateSeatValue = DataContext.Seats.FirstOrDefault(seat => seat.SeatNumber == seatNumber);
                 updateSeatValue.SeatNumber = " X  ";
-                DataContext.Passengers.Add(passenger);
-                Console.WriteLine("Seat booked successfully!");
-                Console.WriteLine("=================================================================");
-                Console.WriteLine($"| Name: {name} | Gender: {gender} | SeatNumber: {seatNumber} | Total Fare: {fare} |");
-                Console.WriteLine("=================================================================");
+                Console.WriteLine(Messages.SeatBookSuccessful);
+                Console.WriteLine($"\n| Name: {name} | Gender: {gender} | SeatNumber: {seatNumber} |  SeatType:{updateSeatValue.SeatType}  | Total Fare: {fare} |\n");
+            }
+            else if(age <= 2)
+            {
+                Console.WriteLine(Messages.AgeBelowTwo);
             }
             else
             {
-                Console.WriteLine("Seat is already booked. Please choose another seat.");
+                Console.WriteLine(Messages.SeatBookUnsuccessful);
             }
         }
         private int CalculateFare(int age)
         {
-            if (age < 2) return 0;
-            if (age < 12) return 50;
+            if (age < 12)
+            {
+                return 50;
+            }
             return 100;
         }
         public void SeatList()
         {
-            Console.WriteLine("Current seat availability:");
-            Console.WriteLine("========================  ================");
-            Console.WriteLine("|   W  ||   M  ||   A  |  |   A  ||   W  |");
-            Console.WriteLine("========================  ================");
+            Console.WriteLine(Messages.SeatListTitleMessage);
             int count = 0;
             foreach (var seat in DataContext.Seats)
             {
@@ -66,7 +65,7 @@
                 }
                 else
                 {
-                    Console.WriteLine("\n----------------------------------------");
+                    Console.WriteLine(Messages.TableLine);
                     count = 0;
                 }
             }
